2376263d7d80704c05ad6d7db2092065e1e5e9f7
Moabe Renato <mrov@cin.ufpe.br>
2021-10-27 07:20:04 -0300

    making a better project structure

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from controllers import dbController
 
 app = Flask(__name__)
 
+app.FLASK_APP = "app.py"
+app.FLASK_ENV = "development"
+app.FLASK_DEBUG = 1
+
 @app.route('/')
 def index():
   return 'Server Works!'
