15d648af05a771eb8b56729ea53f7e87f8847b56
Moabe Renato <mrov@cin.ufpe.br>
2021-10-27 09:53:05 -0300

    more infos on the crawler
    translating the dates from the website to datetime format, and getting location and tech features with a better way

--- controllers/crawler.py
+++ controllers/crawler.py
@@ -5,9 +5,22 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from . systemVariables import chromeDriveLocation, formattedURL
 
+
+def translate_date(date, hour):
+    translated_date = ""
+    if (date.upper() == "HOJE"):
+        translated_date = datetime.now().strftime("%Y %b %d")
+        translated_date = datetime.strptime(translated_date + " " + hour, "%Y %b %d %H:%M")
+    elif (date.upper() == "ONTEM"):
+        translated_date = datetime.now() - timedelta(days=1)
+        translated_date = datetime.strptime(translated_date + " " + hour, "%Y %b %d %H:%M")
+    # TODO para fazer com datas maiores que "ontem"
+    # else:
+    #     datetime.strptime(date + " " + hour, " %H:%M")
+    return translated_date
 
 def configure_driver():
     # Add additional Options to the webdriver
@@ -36,16 +49,24 @@ def getCars(driver, carBrand="", page=1):
 
     for carLink in entirePage.select("li a[data-lurker-detail='list_id']"):
         price = carLink.select_one("span[color='graphite']").text.replace("R$", "").replace(".", "").strip()
+        technicalFeatures = carLink.select("span[color='dark']")[0].text
+        post_date = carLink.select("span[color='dark']")[1].text
+        post_hour = carLink.select("span[color='dark']")[2].text
+        post_location = carLink.select("span[color='dark']")[3].text
+
+        translated_date = translate_date(post_date, post_hour)
 
         if price:
             cars.append({ "announceName": carLink.select_one("h2").text,
                     "formattedPrice": carLink.select_one("span[color='graphite']").text,
                     "price": int(price),
-                    "technicalFeatures": carLink.select_one("span[color='dark']").text,
+                    "technicalFeatures": technicalFeatures,
                     "_id": carLink.attrs['data-lurker_list_id'],
                     "link": carLink.attrs['href'],
                     "img": carLink.select_one("img").attrs["src"],
-                    "created": datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000Z") })
+                    "location": post_location,
+                    "postDate": translated_date,
+                    "created": datetime.now() })
     return cars
 
 if __name__ == "__main__":
