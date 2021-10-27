d295d7763352f85ee22d464369e582655deab17b
Moabe Renato <mrov@cin.ufpe.br>
2021-10-26 23:02:14 -0300

    parameterizing the mongoURL
    now the mongoURL stays on the systemVariables File, better for maintenance

--- dbController.py
+++ dbController.py
@@ -1,16 +1,13 @@
 from pymongo import MongoClient
 from pprint import pprint
-from systemVariables import databaseName
+from systemVariables import databaseName, connectionString
 import pymongo
 import crawler
 
 def get_database():
 
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb://localhost:27017/"
-
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient(CONNECTION_STRING)
+    client = MongoClient(connectionString)
 
     # Create the database for our example (we will use the same database throughout the tutorial
     return client[databaseName]
