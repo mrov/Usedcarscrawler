05f1622138d56700c55b33ee3e576e7b7c4d5227
Moabe Renato <mrov@cin.ufpe.br>
2021-10-26 22:55:48 -0300

    adding the crawler file
    using selenium and beautifulsoup to cralwer over the websites

---
+++ crawler.py
@@ -0,0 +1,55 @@
+# pluralsight.py
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+import time
+import systemVariables
+
+
+def configure_driver():
+    # Add additional Options to the webdriver
+    chrome_options = Options()
+    # add the argument and make the browser Headless.
+    chrome_options.add_argument("--headless")
+    # Instantiate the Webdriver: Mention the executable path of the webdriver you have downloaded
+    # For linux/Mac
+    # driver = webdriver.Chrome(options = chrome_options)
+    # For windows
+    driver = webdriver.Chrome(executable_path="./chromedriver.exe", options = chrome_options)
+    return driver
+
+def getCars(driver, carBrand="", page=1):
+    cars = []
+
+    # Step 1: Go to pluralsight.com, category section with selected search keyword
+    driver.get(systemVariables.formattedURL(carBrand, page))
+    # wait for the element to load
+    try:
+        WebDriverWait(driver, 10).until(lambda s: s.find_element_by_css_selector("#ad-list").is_displayed())
+    except TimeoutException:
+        print("TimeoutException: Element not found")
+        return None
+
+    # Step 2: Create a parse tree of page sources after searching
+    entirePage = BeautifulSoup(driver.page_source, "lxml")
+
+    # Step 3: Iterate over the search result and fetch the course
+    for carLink in entirePage.select("li a[data-lurker-detail='list_id']"):
+        cars.append({ "announceName": carLink.select_one("h2").text,
+                "price": carLink.select_one("span[color='graphite']").text,
+                "technicalFeatures": carLink.select_one("span[color='dark']").text,
+                "_id": carLink.attrs['data-lurker_list_id'],
+                "link": carLink.attrs['href'],
+                "img": carLink.select_one("img").attrs["src"] })
+    return cars
+
+# create the driver object.
+# driver = configure_driver()
+
+# getCars(driver)
+
+# close the driver.
+# driver.close()
