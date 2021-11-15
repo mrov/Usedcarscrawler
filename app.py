534b181bcf9bd69b45e72354621f58dbef60206f
Ubuntu <ubuntu@SeminovosProject.zavsntghy1wuhncsdbtjvx2o4g.bx.internal.cloudapp.net>
2021-11-15 18:47:44 +0000

    adicionando cors ao codigo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from controllers import dbController
 
 app = Flask(__name__)
@@ -7,10 +8,15 @@ app.FLASK_APP = "app.py"
 app.FLASK_ENV = "development"
 app.FLASK_DEBUG = 1
 
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/updateDB')
 def index():
   return dbController.craw_website()
   
 @app.route('/getCars')
+@cross_origin()
 def say_hello():
   return dbController.db_get_cars()
