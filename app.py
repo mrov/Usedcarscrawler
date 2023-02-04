2b02839ad2f22b175a8dc2c9a1475ad02c8e529b
Moabe Renato <mrov@cin.ufpe.br>
2023-02-03 21:38:13 -0300

    test without cors

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import request
-from flask_cors import CORS, cross_origin
 from controllers import dbController
 
 app = Flask(__name__)
@@ -8,8 +7,6 @@ app = Flask(__name__)
 app.FLASK_APP = "app.py"
 app.FLASK_ENV = "development"
 app.FLASK_DEBUG = 1
-
-cors = CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -21,6 +18,5 @@ def index():
 # params startDate
 # params endDate
 @app.route('/getCars')
-@cross_origin()
 def say_hello():
   return dbController.db_get_cars(request)
